a592da89573263cc8cc03bd20b740ddd0a1cad6c
Kavia AI <production@kavia.ai>
2025-12-01 04:43:11 +0000

    Checkpoint 1 - cg494e7ea5

---
+++ task_manager_backend/src/main/java/com/example/demo/taskmanagerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class taskmanagerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(taskmanagerbackendApplication.class, args);
+	}
+
+}
